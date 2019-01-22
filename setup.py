930c4c2bd43d5395a4b29f68446af0bfe8a31bbe
Miikka Värri <miikka.varri@gmail.com>
2019-01-22 20:12:42 +0100

    install_requires uses requriements.txt file

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+def parse_requirements(filename):
+  """ load requirements from a pip requirements file """
+  lineiter = (line.strip() for line in open(filename))
+  return [line for line in lineiter if line and not line.startswith("#")]
+
 with open("README.md", "r") as fh:
   long_description = fh.read()
 
 setup(
   name='ELSPy',
-  version='0.1',
+  version='0.1.1',
   scripts=['elspy.py'] ,
   author="Miikka Värri",
   author_email="miikka.varri@gmail.com",
@@ -13,9 +18,7 @@ setup(
   long_description=long_description,
   long_description_content_type="text/markdown",
   url="https://github.com/ruuti/ELSPy",
-  install_requires=[
-    'zeep>=3.2.0'
-  ],
+  install_requires=parse_requirements('requirements.txt'),
   classifiers=[
      "Programming Language :: Python :: 3",
      "License :: OSI Approved :: MIT License",
